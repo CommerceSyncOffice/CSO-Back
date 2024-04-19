db956da4a414e8038d2894572bc6c1ffe96f8929
wonowonow <wonow_@naver.com>
2024-04-19 14:54:41 +0900

    init commit

---
+++ src/test/java/commercesyncoffice/org/CommerceSyncOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package commercesyncoffice.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceSyncOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
