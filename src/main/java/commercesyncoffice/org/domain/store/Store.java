8e0fc8d2508c507cd592b8faa2c33c2738a864c0
wonowonow <wonow_@naver.com>
2024-04-20 21:49:19 +0900

    add README

---
+++ src/main/java/commercesyncoffice/org/domain/store/Store.java
@@ -0,0 +1,50 @@
+package commercesyncoffice.org.domain.store;
+
+import commercesyncoffice.org.domain.brand.Brand;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String address;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false)
+    private Boolean type;
+
+    @Column(nullable = false)
+    private Float commissionRate;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    private LocalDateTime modifiedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+}
