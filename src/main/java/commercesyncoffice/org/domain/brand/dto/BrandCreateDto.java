d866918b666d7878990c1a4771708f43c3e311d2
wonowonow <wonow_@naver.com>
2024-04-23 16:18:55 +0900

    doc: create DTOs 주석 추가

--- src/main/java/commercesyncoffice/org/domain/brand/dto/BrandCreateDto.java
+++ src/main/java/commercesyncoffice/org/domain/brand/dto/BrandCreateDto.java
@@ -1,6 +1,6 @@
 package commercesyncoffice.org.domain.brand.dto;
 
-
+//TODO 추후 domain 에 따라 validation 추가
 public record BrandCreateDto(
         String name,
         String description
--- src/main/java/commercesyncoffice/org/domain/category/dto/CategoryCreateDto.java
+++ src/main/java/commercesyncoffice/org/domain/category/dto/CategoryCreateDto.java
@@ -1,5 +1,6 @@
 package commercesyncoffice.org.domain.category.dto;
 
+//TODO 추후 domain 에 따라 validation 추가
 public record CategoryCreateDto(
         String name,
         String description
