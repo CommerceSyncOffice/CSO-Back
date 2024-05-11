67497d6e7024266e1c807848fd4ffe58b09463cc
wonowonow <wonow_@naver.com>
2024-05-11 13:07:45 +0900

    fix: querydsl 의존성 삭제

--- src/main/java/commercesyncoffice/org/domain/item/repository/ItemQueryRepository.java
+++
@@ -1,9 +0,0 @@
-//package commercesyncoffice.org.domain.item.repository;
-//
-//import commercesyncoffice.org.domain.item.dto.ItemDetailDto;
-//import java.util.Optional;
-//
-//public interface ItemQueryRepository {
-//
-//    Optional<ItemDetailDto> findByIdCustom(Long itemId);
-//}
