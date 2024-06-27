b87a45649cec831535f88456d8de28a59b7886f7
wonowonow <wonow_@naver.com>
2024-06-27 11:41:22 +0900

    docs: import 삭제

--- src/main/java/commercesyncoffice/org/domain/member/controller/MemberController.java
+++ src/main/java/commercesyncoffice/org/domain/member/controller/MemberController.java
@@ -11,8 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
