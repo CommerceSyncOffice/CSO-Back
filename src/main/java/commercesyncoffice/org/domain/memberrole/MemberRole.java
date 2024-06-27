a4680edc166ae6aa895132eba9f2bc94e688f08f
wonowonow <wonow_@naver.com>
2024-06-27 13:03:49 +0900

    feat: MemberRole 추가, @PostConstruct Init 메서드 추가 (MemberRoleEnum Constraint 삭제 및 등록)

--- src/main/java/commercesyncoffice/org/domain/memberrole/MemberRole.java
+++ src/main/java/commercesyncoffice/org/domain/memberrole/MemberRole.java
@@ -10,9 +10,11 @@ import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
