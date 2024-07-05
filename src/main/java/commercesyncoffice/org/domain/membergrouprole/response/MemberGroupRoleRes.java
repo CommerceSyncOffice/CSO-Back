f8c5de4ba2cad748085b1f56152249e97553ae6f
wonowonow <wonow_@naver.com>
2024-07-05 21:16:11 +0900

    fix: 직렬화 안되던 오류 수정

--- src/main/java/commercesyncoffice/org/domain/membergrouprole/response/MemberGroupRoleRes.java
+++ src/main/java/commercesyncoffice/org/domain/membergrouprole/response/MemberGroupRoleRes.java
@@ -2,7 +2,9 @@ package commercesyncoffice.org.domain.membergrouprole.response;
 
 import commercesyncoffice.org.domain.membergrouprole.dto.response.MemberGroupRoleResDto;
 import commercesyncoffice.org.global.response.CommonResponse;
+import lombok.Getter;
 
+@Getter
 public class MemberGroupRoleRes extends CommonResponse {
 
     private final MemberGroupRoleResDto memberGroupRoleResDto;
